2f6334feea7b0b4d9d3b5db8dd5cb8afd25b81d5
liatbenshabatruni <liat.benshabat@post.runi.ac.il>
2025-01-10 13:51:49 +0200

    adding to lower case

--- User.java
+++ User.java
@@ -43,13 +43,20 @@
 
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
+        //name = name.toLowerCase();
         if (this.follows==null){
             return false;
         }
+        if (name==null){
+            return false;
+        }
         for (int i = 0; i < this.follows.length; i++) {
-            if((this.follows[i]!=null)&&(this.follows[i].equals(name))){
-                  //if the name is found in the array - return true 
-                  return true; 
+            if((this.follows[i]!=null)){
+                if (this.follows[i].toLowerCase().equals(name.toLowerCase())){
+                    //if the name is found in the array - return true 
+                    return true;
+                }
+                    
               }
             }
         return false;
@@ -57,23 +64,19 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
+        //name = name.toLowerCase();
         //if the number of the null follows is 10 - the list is full
         //if (this.follows(null)==false){return false;}
 
         //if the user is already in the array
-        if (this.follows(name)==true){
+        if ((this.follows(name)==true)||(this.fCount==10)){
             return false;
         }
         else {
-            for (int i = 0; i < this.follows.length; i++) {
-                if (this.follows[i]==null){
-                    this.follows[i]=name;
-                    this.fCount++;
-                    return true; 
-                }
-            }
+            this.follows[fCount]=name;
+            this.fCount++;
+            return true;
         }
-        return false;
     }
 
     /** Removes the given name from the follows list of this user. If successful, returns true.
