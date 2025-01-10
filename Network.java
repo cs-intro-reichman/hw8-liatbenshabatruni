fea5bde85db4e8bc43587e11b260d30f98253b3c
liatbenshabatruni <liat.benshabat@post.runi.ac.il>
2025-01-10 15:19:11 +0200

    Testing Network second  time

--- Network.java
+++ Network.java
@@ -44,7 +44,7 @@ public class Network {
     *  If the given name is already a user in this network, does nothing and returns false;
     *  Otherwise, creates a new user with the given name, adds the user to this network, and returns true. */
     public boolean addUser(String name) {
-        if ((getUser(name)!=null)&&(this.userCount>=users.length)) {
+        if ((getUser(name)!=null)&&(this.userCount>=1000)) {
             return false;
         }
         else {
