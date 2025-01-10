bcecdf77e01895faf131f7c65129444e3728c750
liatbenshabatruni <liat.benshabat@post.runi.ac.il>
2025-01-10 15:35:27 +0200

    again 2

--- Network.java
+++ Network.java
@@ -31,6 +31,9 @@ public class Network {
      *  If there is no such user, returns null.
      *  Notice that the method receives a String, and returns a User object. */
     public User getUser(String name) {
+        if (name==null){
+            return null;
+        }
         for (int i = 0; i < this.userCount; i++) {
             if (users[i].getName().toLowerCase().equals(name.toLowerCase())){
                 return users[i];
@@ -44,6 +47,9 @@ public class Network {
     *  If the given name is already a user in this network, does nothing and returns false;
     *  Otherwise, creates a new user with the given name, adds the user to this network, and returns true. */
     public boolean addUser(String name) {
+        if (name==null){
+            return false;
+        }
         if ((getUser(name)!=null)||(this.userCount==users.length)) {
             return false;
         }
@@ -58,6 +64,9 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
+        if ((name1==null)||(name2==null)){
+            return false;
+        }
         if ((getUser(name1)==null)||(getUser(name2)==null)||(name1.toLowerCase().equals(name2.toLowerCase()))) {
             return false;
         } 
@@ -67,6 +76,9 @@ public class Network {
     /** For the user with the given name, recommends another user to follow. The recommended user is
      *  the user that has the maximal mutual number of followees as the user with the given name. */
     public String recommendWhoToFollow(String name) {
+        if (name==null){
+            return null;
+        }
         int maxFollowers = 0;
         int index = 0;
         for (int i = 0; i < userCount; i++) {
@@ -104,6 +116,9 @@ public class Network {
     /** Returns the number of times that the given name appears in the follows lists of all
      *  the users in this network. Note: A name can appear 0 or 1 times in each list. */
     private int followeeCount(String name) {
+        if (name==null){
+            return 0;
+        }
         int counter = 0;
         for (int i = 0; i < userCount; i++) {
             if(users[i].follows(name))
