a81f26366d8a425d74db7309fe9722646f43f0c4
liatbenshabatruni <liat.benshabat@post.runi.ac.il>
2025-01-10 16:06:16 +0200

    again 7

--- Network.java
+++ Network.java
@@ -96,9 +96,6 @@ public class Network {
         int counter = 0;
         int maxFollows=0;
         int index = 0;
-        if(users==null){
-            return null;
-        }
         for (int i = 0; i < userCount; i++) {
             for (int j = 0; j < userCount; j++) {
                 if ((this.users[i]==null)||(users[j]==null)){
@@ -115,6 +112,9 @@ public class Network {
                 index=i;
             }
             counter=0;
+        }
+        if(users[index]==null){
+            return null;
         }
         return users[index].getName();
     }
